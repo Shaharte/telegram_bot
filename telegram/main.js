dd23cead51d591e7b25c4f504d90df1cbb128f4e
Shahar Terem <shaharmccann@gmail.com>
2021-02-13 10:46:29 +0200

    updates

--- telegram/main.js
+++ telegram/main.js
@@ -664,7 +664,7 @@ module.exports.Shishit = async function () {
 
 
     }
-    nodeSchedule.scheduleJob('* 15-21 * * *', () => {
+    nodeSchedule.scheduleJob('* 12-21 * * *', () => {
         try {
             scraper()
         } catch (err) { }
